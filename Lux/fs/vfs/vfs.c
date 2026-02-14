2e6beb3085644fc6192135455be9432c2ab95dc2
qwayer <mazurkevicdenis05@gmail.com>
2026-02-14 12:18:35 +0300

    исправлена vfs, добавленны системные вызовы(для ring 3)

--- Lux/fs/vfs/vfs.c
+++ Lux/fs/vfs/vfs.c
@@ -1,43 +1,49 @@
 #include "vfs.h"
 
-struct vfs_node* vfs_root = 0; 
+struct vfs_node* vfs_root = 0;
 
 int read_vfs(struct vfs_node* node, unsigned int offset, unsigned int size, char* buffer) {
-    if (node && node->read) {
+    if (node && node->read)
         return node->read(node, offset, size, buffer);
-    }
-    return 0;
+    return -1;
 }
 
 int write_vfs(struct vfs_node* node, unsigned int offset, unsigned int size, char* buffer) {
-    if (node && node->write) {
+    if (node && node->write)
         return node->write(node, offset, size, buffer);
-    }
-    return 0;
+    return -1;
 }
 
 void open_vfs(struct vfs_node* node) {
-    if (node && node->open) {
+    if (node && node->open)
         node->open(node);
-    }
 }
 
 void close_vfs(struct vfs_node* node) {
-    if (node && node->close) {
+    if (node && node->close)
         node->close(node);
-    }
 }
 
 struct vfs_dirent* readdir_vfs(struct vfs_node* node, unsigned int index) {
-    if (node && (node->type & VFS_DIRECTORY) && node->readdir) {
+    if (node && (node->type & VFS_DIRECTORY) && node->readdir)
         return node->readdir(node, index);
-    }
     return 0;
 }
 
 struct vfs_node* finddir_vfs(struct vfs_node* node, char* name) {
-    if (node && (node->type & VFS_DIRECTORY) && node->finddir) {
+    if (node && (node->type & VFS_DIRECTORY) && node->finddir)
         return node->finddir(node, name);
-    }
     return 0;
+}
+
+int create_vfs(struct vfs_node* node, char* name) {
+    if (node && (node->type & VFS_DIRECTORY) && node->create)
+        return node->create(node, name);
+    return -1;
+}
+
+int delete_vfs(struct vfs_node* node, char* name) {
+    if (node && (node->type & VFS_DIRECTORY) && node->delete)
+        return node->delete(node, name);
+    return -1;
 }
