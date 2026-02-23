d59aa3210b278c101a248c7c15329c703fd322a3
QwaYer <mazurkevicdenis05@gmail.com>
2026-02-23 19:51:42 +0300

    Переход на grub2

--- Lux/kernel/core/kernel.asm
+++ Lux/kernel/core/kernel.asm
@@ -1,17 +1,41 @@
 [bits 32]
 [global _start]
 
+; Multiboot header
+MODULEALIGN equ  1<<0
+MEMINFO     equ  1<<1
+FLAGS       equ  MODULEALIGN | MEMINFO
+MAGIC       equ  0x1BADB002
+CHECKSUM    equ -(MAGIC + FLAGS)
+
+section .multiboot
+align 4
+    dd MAGIC
+    dd FLAGS
+    dd CHECKSUM
+
+section .bss
+align 16
+stack_bottom:
+    resb 16384 ; 16 KB
+stack_top:
+
+section .text
 extern init
 
 _start:
-    mov ax, 0x10        ; DATA_SEG
-    mov ds, ax
-    mov ss, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-    mov ebp, 0x90000
-    mov esp, ebp
+    mov esp, stack_top
+    
+    ; Reset EFLAGS
+    push 0
+    popf
+
+    ; Push multiboot info
+    push ebx
+    push eax
+
     call init
 halt:
+    cli
+    hlt
     jmp halt
