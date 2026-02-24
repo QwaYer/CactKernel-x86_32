b08b43f024e177e1843740ea60704f696e24af11
qwayer <mazurkevicdenis05@gmail.com>
2026-02-24 21:41:30 +0300

    Все 31 исключения процессора интегрированы

--- Lux/kernel/core/interrupt.asm
+++ Lux/kernel/core/interrupt.asm
@@ -6,8 +6,37 @@ global timer_isr
 global syscall_isr
 global isr_common_stub
 global isr0
+global isr1
+global isr2
+global isr3
+global isr4
+global isr5
+global isr6
+global isr7
+global isr8
+global isr9
+global isr10
+global isr11
+global isr12
 global isr13
 global isr14
+global isr15
+global isr16
+global isr17
+global isr18
+global isr19
+global isr20
+global isr21
+global isr22
+global isr23
+global isr24
+global isr25
+global isr26
+global isr27
+global isr28
+global isr29
+global isr30
+global isr31
 
 extern keyboard_handler
 extern virtio_net_irq_handler
@@ -29,16 +58,153 @@ timer_ticks_get:
 
 
 isr0:
-    push dword 0   
-    push dword 0   
+    push dword 0
+    push dword 0
+    jmp isr_common_stub
+
+isr1:
+    push dword 0
+    push dword 1
+    jmp isr_common_stub
+
+isr2:
+    push dword 0
+    push dword 2
+    jmp isr_common_stub
+
+isr3:
+    push dword 0
+    push dword 3
+    jmp isr_common_stub
+
+isr4:
+    push dword 0
+    push dword 4
+    jmp isr_common_stub
+
+isr5:
+    push dword 0
+    push dword 5
+    jmp isr_common_stub
+
+isr6:
+    push dword 0
+    push dword 6
+    jmp isr_common_stub
+
+isr7:
+    push dword 0
+    push dword 7
+    jmp isr_common_stub
+
+isr8:
+    push dword 8
+    jmp isr_common_stub
+
+isr9:
+    push dword 0
+    push dword 9
+    jmp isr_common_stub
+
+isr10:
+    push dword 10
+    jmp isr_common_stub
+
+isr11:
+    push dword 11
+    jmp isr_common_stub
+
+isr12:
+    push dword 12
     jmp isr_common_stub
 
 isr13:
-    push dword 13 
+    push dword 13
     jmp isr_common_stub
 
 isr14:
-    push dword 14 
+    push dword 14
+    jmp isr_common_stub
+
+isr15:
+    push dword 0
+    push dword 15
+    jmp isr_common_stub
+
+isr16:
+    push dword 0
+    push dword 16
+    jmp isr_common_stub
+
+isr17:
+    push dword 17
+    jmp isr_common_stub
+
+isr18:
+    push dword 0
+    push dword 18
+    jmp isr_common_stub
+
+isr19:
+    push dword 0
+    push dword 19
+    jmp isr_common_stub
+
+isr20:
+    push dword 0
+    push dword 20
+    jmp isr_common_stub
+
+isr21:
+    push dword 21
+    jmp isr_common_stub
+
+isr22:
+    push dword 0
+    push dword 22
+    jmp isr_common_stub
+
+isr23:
+    push dword 0
+    push dword 23
+    jmp isr_common_stub
+
+isr24:
+    push dword 0
+    push dword 24
+    jmp isr_common_stub
+
+isr25:
+    push dword 0
+    push dword 25
+    jmp isr_common_stub
+
+isr26:
+    push dword 0
+    push dword 26
+    jmp isr_common_stub
+
+isr27:
+    push dword 0
+    push dword 27
+    jmp isr_common_stub
+
+isr28:
+    push dword 0
+    push dword 28
+    jmp isr_common_stub
+
+isr29:
+    push dword 29
+    jmp isr_common_stub
+
+isr30:
+    push dword 30
+    jmp isr_common_stub
+
+isr31:
+    push dword 0
+    push dword 31
     jmp isr_common_stub
 
 ; порядок на стеке (от младших адресов к старшим):
